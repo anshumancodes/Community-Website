b8009e8e4112cff774143f4994b0c1339c264629
anshumancodes <anshumanprof01@gmail.com>
2023-08-06 11:41:12 +0530

    fixed mobile toggle menu error in app.js and style.css

--- logic/app.js
+++ logic/app.js
@@ -1,9 +1,10 @@
 // toggle navar on mobile
-const links = document.querySelector(".links");
-const navToggle = document.querySelector("#nav-toggle");
+let links = document.querySelector(".links");
+let navToggle = document.querySelector("#nav-toggle");
 console.log(navToggle);
 console.log(links);
 
-navToggle.addEventListener("click", () => {
-  links.classList.toggle("active");
+navToggle.addEventListener("click",()=>{
+links.classList.toggle("active");
 });
+  
